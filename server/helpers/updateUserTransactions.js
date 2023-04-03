42de3f07a5d271c8a2bbfb5d677293aeff270d7a
Nita1001 <devinedevops@gmail.com>
2023-04-03 09:07:10 +0300

    updateUserTransactions

--- server/helpers/updateUserTransactions.js
+++ server/helpers/updateUserTransactions.js
@@ -1,24 +1,37 @@
 const Transaction = require('../models/transactionSchema');
 
-async function updateUserTransactions(fromAccountId, toAccountId, transactionId) {
-    try {
-        await Transaction.updateOne(
-            { accountId: fromAccountId },
-            { $push: { transactions: { _id: transactionId, to: toAccountId } } }
-        );
-    } catch (error) {
-        console.error(error);
-        throw new Error('Something went wrong');
-    }
+const updateUserTransactions = async (fromAccountId, toAccountId, transactionId) => {
+    if (fromAccountId === toAccountId) {
+        try {
+            await Transaction.updateOne(
+                { accountId: toAccountId },
+                { $push: { transactions: { _id: transactionId, from: fromAccountId } } }
+            );
+        } catch (error) {
+            console.error(error);
+            throw new Error('Something went wrong');
+        }
 
-    try {
-        await Transaction.updateOne(
-            { accountId: toAccountId },
-            { $push: { transactions: { _id: transactionId, from: fromAccountId } } }
-        );
-    } catch (error) {
-        console.error(error);
-        throw new Error('Something went wrong');
+    } else {
+        try {
+            await Transaction.updateOne(
+                { accountId: fromAccountId },
+                { $push: { transactions: { _id: transactionId, to: toAccountId } } }
+            );
+        } catch (error) {
+            console.error(error);
+            throw new Error('Something went wrong');
+        }
+
+        try {
+            await Transaction.updateOne(
+                { accountId: toAccountId },
+                { $push: { transactions: { _id: transactionId, from: fromAccountId } } }
+            );
+        } catch (error) {
+            console.error(error);
+            throw new Error('Something went wrong');
+        }
     }
 }
 
