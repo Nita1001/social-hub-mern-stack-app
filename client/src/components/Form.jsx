d3ae2b3f227858e178c0ec45b7e2b1c569ddf41d
Nita1001 <devinedevops@gmail.com>
2023-04-08 19:27:26 +0300

    register

--- client/src/components/Form.jsx
+++ client/src/components/Form.jsx
@@ -1,6 +1,7 @@
 import React, { useContext } from "react";
 import "./styles/Form.style.css";
 import { LoginContext } from "../contexts/LoginContext";
+import { register } from "../api/userServices";
 
 const Form = ({ type }) => {
     const { login } = useContext(LoginContext);
@@ -11,14 +12,54 @@ const Form = ({ type }) => {
         const password = document.getElementById("password").value;
         login(email, password);
     };
+
+    const handleSubmitRegister = async (event) => {
+        event.preventDefault();
+        const firstName = document.getElementById("firstName").value;
+        const lastName = document.getElementById("lastName").value;
+        const username = document.getElementById("username").value;
+        const phone = document.getElementById("phone").value;
+        const location = document.getElementById("location").value;
+        const email = document.getElementById("email").value;
+        const password = document.getElementById("password").value;
+
+        register(
+            firstName,
+            lastName,
+            username,
+            phone,
+            location,
+            email,
+            password
+        );
+    };
+
     return (
         <div className="container">
             <form>
                 {type === "register" && (
                     <>
-                        <input type="text" id="name" placeholder="First Name" />
-                        <input type="text" id="name" placeholder="Last Name" />
-                        <input type="text" id="name" placeholder="username" />
+                        <input
+                            type="text"
+                            id="firstName"
+                            placeholder="First Name"
+                        />
+                        <input
+                            type="text"
+                            id="lastName"
+                            placeholder="Last Name"
+                        />
+                        <input
+                            type="text"
+                            id="username"
+                            placeholder="username"
+                        />
+                        <input type="number" id="phone" placeholder="Phone" />
+                        <input
+                            type="text"
+                            id="location"
+                            placeholder="Location"
+                        />
                     </>
                 )}
                 <input type="email" id="email" placeholder="email" />
@@ -26,9 +67,13 @@ const Form = ({ type }) => {
                 <button
                     type="submit"
                     id="login-btn"
-                    onClick={handleSubmitLogin}
+                    onClick={
+                        type === "register"
+                            ? handleSubmitRegister
+                            : handleSubmitLogin
+                    }
                 >
-                    Login
+                    {type === "register" ? "Register" : "Login"}
                 </button>
             </form>
         </div>
