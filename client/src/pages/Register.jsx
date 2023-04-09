f7f45b548b9bcb24f1b608afd3c25d27ab701bec
Nita1001 <devinedevops@gmail.com>
2023-04-09 15:00:46 +0300

    Register page

--- client/src/pages/Register.jsx
+++ client/src/pages/Register.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import RegisterForm from "../components/RegisterForm";
 import { Link } from "react-router-dom";
+import "./styles/Login.style.css";
+
 const Register = () => {
     return (
-        <>
+        <div className="login-page-container">
             <RegisterForm />
-            <Link to="/register">Already a member?</Link>
-        </>
+            <Link to="/login">Already a member?</Link>
+        </div>
     );
 };
 
