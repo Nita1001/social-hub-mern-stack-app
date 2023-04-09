96ca065190e9de7da9bba18a9a70f7a3b82211bd
Nita1001 <devinedevops@gmail.com>
2023-04-09 15:00:52 +0300

    Login page

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,10 +1,11 @@
 import React from "react";
 import LoginForm from "../components/LoginForm";
 import { Link } from "react-router-dom";
+import "./styles/Login.style.css";
 
 const Login = () => {
     return (
-        <div className="form-container">
+        <div className="login-page-container">
             <LoginForm />
             <Link to="/register">Not registered?</Link>
         </div>
