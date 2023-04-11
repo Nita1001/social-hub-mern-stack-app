21d8f42039f7ff979406ed2a2f40fd6d6a225569
Nita1001 <devinedevops@gmail.com>
2023-04-11 13:03:25 +0300

    updateMessages

--- server/helpers/updateMessages.js
+++ server/helpers/updateMessages.js
@@ -1,9 +1,9 @@
 
-const User = require('../models/userSchema.js');
+const Conversation = require('../models/conversationSchema.js');
 
 const updateMessages = async (accountId, update) => {
     try {
-        const updatedMessages = await User.findByIdAndUpdate(accountId, update, { new: true });
+        const updatedMessages = await Conversation.findByIdAndUpdate(accountId, update, { new: true });
         console.log('3333', updatedMessages);
         return updatedMessages;
     } catch (error) {
