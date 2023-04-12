0c8c59d7aecf74fc53da1cb7c234a7090ebbf7ab
Nita1001 <devinedevops@gmail.com>
2023-04-12 19:13:17 +0300

    Messages route

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,5 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import { Login, Register, Profile, NotFound, Home } from "./pages";
+import { Login, Register, Profile, NotFound, Home, Messages } from "./pages";
 import SharedLayout from "./components/layout/SharedLayout";
 
 import "./App.css";
@@ -12,6 +12,7 @@ function App() {
                     <Route index element={<Home />} />
                     <Route path="/login" element={<Login />} />
                     <Route path="/register" element={<Register />} />
+                    <Route path="/messages" element={<Messages />} />
                     <Route path="/profile" element={<Profile />} />
                     <Route path="*" element={<NotFound />} />
                 </Route>
