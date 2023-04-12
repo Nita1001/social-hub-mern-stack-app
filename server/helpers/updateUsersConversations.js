446848fe68a860266731e6daa9d2e91a03349d9d
Nita1001 <devinedevops@gmail.com>
2023-04-12 19:03:56 +0300

    update conversations array

--- server/helpers/updateUsersConversations.js
+++ server/helpers/updateUsersConversations.js
@@ -1,15 +1,14 @@
 
-const User = require('../models/inventorySchema.js');
+const User = require('../models/userSchema.js');
 
-const updateUsersConversations = async (userId, update) => {
+exports.updateUsersConversations = async (userId, update) => {
     try {
         const user = await User.findByIdAndUpdate(userId, update, { new: true });
         const updatedConversations = user.conversations;
         return updatedConversations;
     } catch (error) {
-        console.error('Error updating user after creating item');
+        console.error(`Error updating User, user not found with id ${userId}`);
         throw new Error(error.message);
     }
 }
 
-module.exports = updateUsersConversations;
