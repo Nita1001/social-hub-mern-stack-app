830790f5736b4208a3652bafb5a9cd09f41fa168
Nita1001 <devinedevops@gmail.com>
2023-04-13 12:25:45 +0300

    selectedUserProvider

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -3,11 +3,14 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./index.css";
 import { LoginProvider } from "./contexts/LoginContext";
+import { SelectedUserProvider } from "./contexts/SelectedUserContext";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
     <React.StrictMode>
         <LoginProvider>
-            <App />
+            <SelectedUserProvider>
+                <App />
+            </SelectedUserProvider>
         </LoginProvider>
     </React.StrictMode>
 );
