04af04357874f9e2116c42a78c59cbe93f3aa078
Nita1001 <devinedevops@gmail.com>
2023-04-13 12:24:22 +0300

    getConversationById

--- server/routes/conversations.js
+++ server/routes/conversations.js
@@ -1,14 +1,17 @@
 const express = require('express');
 const router = express.Router();
-const { createConversation, getConversationByUserId, getUsersConversation } = require('../controllers/conversationController');
+const { createConversation, getConversationByUserId, getUsersConversation, getConversationById } = require('../controllers/conversationController');
 
 // Create a new conversation
 router.post('/', createConversation);
 
 // Get all conversations for a user
 router.get('/:userId', getConversationByUserId);
-
+// Get conversation by Id
+router.get('/users/:conversationId', getConversationById);
 // Get conversation between 2 users 
 router.get('/:userAId/:userBId', getUsersConversation);
 
 module.exports = router;
+
+// 64370c1783d537a2fff20ba4
