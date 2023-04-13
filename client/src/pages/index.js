1a9f7a9c03375ba3f30f404d030fd55d3da491cb
Nita1001 <devinedevops@gmail.com>
2023-04-13 12:21:42 +0300

    Messages Page

--- client/src/pages/index.js
+++ client/src/pages/index.js
@@ -4,5 +4,6 @@ import Login from './Login';
 import Profile from './Profile';
 import NotFound from './NotFound';
 import Home from './Home';
+import Messages from './Messages';
 
-export { Register, Login, Profile, NotFound, Home };
+export { Register, Login, Profile, NotFound, Home, Messages };
