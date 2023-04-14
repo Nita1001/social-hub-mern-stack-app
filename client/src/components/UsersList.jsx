8188c86318495b791edba4b219f69c9ef9181823
Nita1001 <devinedevops@gmail.com>
2023-04-14 14:28:14 +0300

    Friends List

--- client/src/components/UsersList.jsx
+++ client/src/components/UsersList.jsx
@@ -1,9 +1,10 @@
-import React, { useEffect, useState } from "react";
-import { useContext } from "react";
-import { LoginContext } from "../contexts/LoginContext.jsx";
+import React, { useEffect, useState, useContext } from "react";
+
 import { getUsers } from "../api/userServices.js";
-import "./styles/usersList.style.css";
+import { LoginContext } from "../contexts/LoginContext.jsx";
 import { SelectedUserContext } from "../contexts/SelectedUserContext.jsx";
+
+import "./styles/usersList.style.css";
 
 const UsersList = () => {
     const [users, setUsers] = useState([]);
@@ -14,19 +15,18 @@ const UsersList = () => {
         const fetchUsers = async () => {
             const response = await getUsers();
             const users = response.filter((user) => user._id !== userId);
-            console.log("users", users);
             setUsers(users);
         };
-
+        console.log("rendered 1");
         fetchUsers();
-    }, []);
+    }, [userId]);
 
     const handleClick = (user) => {
         handleUserSelected(user);
     };
     return (
         <div>
-            <h2>Users List</h2>
+            <h2>Friends</h2>
             <ul className="ul-container">
                 {users.map((user) => (
                     <li
