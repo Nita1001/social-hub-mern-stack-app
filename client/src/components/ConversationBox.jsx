e4a8a9717ddbecd8e71f5cfe826fbc98f7b46f43
Nita1001 <devinedevops@gmail.com>
2023-04-14 14:27:06 +0300

    child component

--- client/src/components/ConversationBox.jsx
+++ client/src/components/ConversationBox.jsx
@@ -5,11 +5,11 @@ import { LoginContext } from "../contexts/LoginContext";
 import { SelectedUserContext } from "../contexts/SelectedUserContext";
 import useConversation from "../hooks/useConversation";
 import Message from "./Message";
+import "./styles/conversation.style.css";
 
-const ConversationBox = () => {
+const ConversationBox = ({ filteredMessages, currentUser }) => {
     const { userData: currentUserData } = useContext(LoginContext);
     const { selectedUser } = useContext(SelectedUserContext);
-    const { filteredMessages, currentUser } = useConversation();
     const conversationBoxRef = useRef(null);
 
     const scrollToBottom = () => {
@@ -24,24 +24,24 @@ const ConversationBox = () => {
 
     return (
         <div className="conversation-box" ref={conversationBoxRef}>
-            {/* TODO: replace with icon/image of the users */}
             <h6>
                 {currentUserData.username} In Conversation with{" "}
                 {selectedUser.username}
             </h6>
-            {filteredMessages?.map(
-                (message, index) =>
-                    // message.message !! I should change to message
-                    message.message.from && (
-                        <Message
-                            key={uniqid()}
-                            message={message.message}
-                            sentByCurrUser={
-                                message.message.from === currentUser
-                            }
-                        />
-                    )
-            )}
+            {filteredMessages &&
+                filteredMessages.map(
+                    (message) =>
+                        // message.message !! I should change to message
+                        message.message.from && (
+                            <Message
+                                key={uniqid()}
+                                message={message.message}
+                                sentByCurrUser={
+                                    message.message.from === currentUser
+                                }
+                            />
+                        )
+                )}
         </div>
     );
 };
