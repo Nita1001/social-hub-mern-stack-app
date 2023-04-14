cf8a66c9f3bd63711d20561e7d70527062c21ed6
Nita1001 <devinedevops@gmail.com>
2023-04-14 14:29:16 +0300

    Messages Page

--- client/src/pages/Messages.jsx
+++ client/src/pages/Messages.jsx
@@ -1,8 +1,16 @@
 import React from "react";
 import Conversation from "../components/Conversation";
+import UsersList from "../components/UsersList";
+
+import "./styles/Messages.style.css";
 
 const Messages = () => {
-    return <Conversation />;
+    return (
+        <div className="UserMessages-container">
+            <UsersList />
+            <Conversation />
+        </div>
+    );
 };
 
 export default Messages;
