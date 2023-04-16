bcc60e18f1eb3551bb95caa8e6efb0250c5083e5
Nita1001 <devinedevops@gmail.com>
2023-04-16 17:07:53 +0300

    loggedIn sidebar

---
+++ client/src/components/layout/LoggedInSidebar.jsx
@@ -0,0 +1,44 @@
+import React, { useContext } from "react";
+import { Link } from "react-router-dom";
+import { LoginContext } from "../../contexts/LoginContext.jsx";
+
+const LoggedInSidebar = () => {
+    const { logout } = useContext(LoginContext);
+
+    return (
+        <>
+            <li className="sidebar-menu-items">
+                <Link to="/profile" className="sidebar-menu-link">
+                    <i className="fa-solid fa-user"></i>{" "}
+                    <span className="sidebar-menu-text">Profile</span>
+                </Link>
+            </li>
+            <li className="sidebar-menu-items">
+                <Link to="/messages" className="sidebar-menu-link">
+                    <i className="fas fa-envelope"></i>
+                    {/* <div className="chatBubble"></div> */}
+                    <span className="sidebar-menu-text">Messages</span>
+                </Link>
+            </li>
+            <li className="sidebar-menu-items">
+                <Link to="/inventory" className="sidebar-menu-link">
+                    <i className="fa-solid fa-box-archive"></i>
+                    {/* <div className="shield"></div> */}
+                    <span className="sidebar-menu-text">Inventory</span>
+                </Link>
+            </li>
+            <li className="sidebar-menu-items">
+                <Link
+                    to="/login"
+                    className="sidebar-menu-link"
+                    onClick={logout}
+                >
+                    <i className="fa-solid fa-arrow-right-from-bracket fa-rotate-180"></i>
+                    <span className="sidebar-menu-text">Log Out</span>
+                </Link>
+            </li>
+        </>
+    );
+};
+
+export default LoggedInSidebar;
--- client/src/components/layout/Sidebar.jsx
+++ client/src/components/layout/Sidebar.jsx
@@ -1,10 +1,12 @@
-import React, { useState } from "react";
+import React, { useContext, useState } from "react";
 import { Link } from "react-router-dom";
+import { LoginContext } from "../../contexts/LoginContext.jsx";
 import "../styles/Sidebar.style.css";
+import LoggedInSidebar from "./LoggedInSidebar.jsx";
 
 const Sidebar = () => {
     const [showSidebar, setShowSidebar] = useState(false);
-
+    const { isLoggedIn } = useContext(LoginContext);
     const handleToggleSidebar = () => {
         setShowSidebar(!showSidebar);
     };
@@ -12,7 +14,7 @@ const Sidebar = () => {
     return (
         <div className={`sidebar ${showSidebar ? "active" : ""}`}>
             <button className="sidebar-toggle" onClick={handleToggleSidebar}>
-                <i className="fas fa-bars"></i>
+                {/* <i className="fas fa-bars"></i> */}
             </button>
             <ul className="sidebar-menu">
                 <li className="sidebar-menu-item">
@@ -21,24 +23,15 @@ const Sidebar = () => {
                         <span className="sidebar-menu-text">Home</span>
                     </Link>
                 </li>
-                <li className="sidebar-menu-item">
-                    <Link to="/login" className="sidebar-menu-link">
-                        <i className="fas fa-sign-in-alt"></i>
-                        <span className="sidebar-menu-text">Login</span>
-                    </Link>
-                </li>
-                <li className="sidebar-menu-item">
-                    <Link to="/register" className="sidebar-menu-link">
-                        <i className="fas fa-user-plus"></i>
-                        <span className="sidebar-menu-text">Register</span>
-                    </Link>
-                </li>
-                <li className="sidebar-menu-items">
-                    <Link to="/messages" className="sidebar-menu-link">
-                        <i className="fas fa-envelope"></i>
-                        <span className="sidebar-menu-text">Messages</span>
-                    </Link>
-                </li>
+                {!isLoggedIn && (
+                    <li className="sidebar-menu-item">
+                        <Link to="/login" className="sidebar-menu-link">
+                            <i className="fas fa-sign-in-alt"></i>
+                            <span className="sidebar-menu-text">Login</span>
+                        </Link>
+                    </li>
+                )}
+                {isLoggedIn && <LoggedInSidebar />}
             </ul>
         </div>
     );
