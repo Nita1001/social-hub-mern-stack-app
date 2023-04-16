8e3728ffe49ff01f1ed06eb3ce907f88ca386c43
Nita1001 <devinedevops@gmail.com>
2023-04-16 20:32:31 +0300

    Storage Page

--- client/src/pages/index.js
+++ client/src/pages/index.js
@@ -5,5 +5,6 @@ import Profile from './Profile';
 import NotFound from './NotFound';
 import Home from './Home';
 import Messages from './Messages';
+import Storage from './Storage';
 
-export { Register, Login, Profile, NotFound, Home, Messages };
+export { Register, Login, Profile, NotFound, Home, Messages, Storage };
