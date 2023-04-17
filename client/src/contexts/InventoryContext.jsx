925f2f93f5e47a6bc0f8a9d24f284668105c55be
Nita1001 <devinedevops@gmail.com>
2023-04-17 21:56:18 +0300

    InventoryContextProvider

---
+++ client/src/contexts/InventoryContext.jsx
@@ -0,0 +1,15 @@
+import React, { createContext, useContext } from "react";
+import useInventory from "../hooks/useInventory";
+
+const InventoryContext = createContext();
+
+const InventoryProvider = ({ children }) => {
+    const inventory = useInventory();
+    return (
+        <InventoryContext.Provider value={inventory}>
+            {children}
+        </InventoryContext.Provider>
+    );
+};
+export const useInventoryContext = () => useContext(InventoryContext);
+export { InventoryContext, InventoryProvider };
--- client/src/main.jsx
+++ client/src/main.jsx
@@ -5,13 +5,16 @@ import "./index.css";
 import { LoginProvider } from "./contexts/LoginContext";
 import { SelectedUserProvider } from "./contexts/SelectedUserContext";
 import DarkModeProvider from "./contexts/DarkModeContext";
+import { InventoryProvider } from "./contexts/InventoryContext";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
     <React.StrictMode>
         <LoginProvider>
             <SelectedUserProvider>
                 <DarkModeProvider>
-                    <App />
+                    <InventoryProvider>
+                        <App />
+                    </InventoryProvider>
                 </DarkModeProvider>
             </SelectedUserProvider>
         </LoginProvider>
