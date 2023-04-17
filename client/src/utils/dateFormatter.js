e9f493bb6c420d4fd28729a71cabb3f82ac84dd4
Nita1001 <devinedevops@gmail.com>
2023-04-17 21:58:49 +0300

    cleared console logs

--- client/src/utils/dateFormatter.js
+++ client/src/utils/dateFormatter.js
@@ -1,5 +1,4 @@
 export const dateFormatter = (updatedAt) => {
-    console.log('updatedAt', updatedAt)
     const date = new Date(updatedAt);
     const options = {
         year: "numeric",
@@ -9,12 +8,10 @@ export const dateFormatter = (updatedAt) => {
         minute: "numeric",
     };
     const formattedDate = new Intl.DateTimeFormat('en-US', options).format(date);
-    console.log('formattedDate', formattedDate)
+    // console.log('formattedDate', formattedDate)
 
     const [, dateString, time] = formattedDate.match(/(.+),\s(.+)/);
     const [, hour, minute, amOpm] = time.match(/(\d+):(\d+)\s(.+)/);
-
-    console.log('year dateString, hour, minute, amOpm', dateString, hour, minute, amOpm);
 
     return { hour, minute, amOpm };
 }
