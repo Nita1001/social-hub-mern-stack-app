bc9f349baf5c01eb4ccf10f110f3a375800ee7f6
Nita1001 <devinedevops@gmail.com>
2023-04-17 22:08:29 +0300

    tempo hardCoded

---
+++ client/src/components/items.js
@@ -0,0 +1,20 @@
+export const items1 = [
+    {
+        id: 14,
+        name: "Item 1",
+        description: "This is item 1",
+        status: "trade",
+    },
+    {
+        id: 24,
+        name: "Item 2",
+        description: "This is item 2",
+        status: "trade",
+    },
+    {
+        id: 34,
+        name: "Item 3",
+        description: "This is item 3",
+        status: "normal",
+    },
+];
---
+++ client/src/components/users.js
@@ -0,0 +1,86 @@
+export const users = [
+    {
+        id: 11,
+        name: "Donna Don",
+        email: "donna.doe@example.com",
+        items: [
+            {
+                id: 1,
+                name: "Item 1",
+                description: "Description 1",
+                status: "trade",
+                userId: 11
+            },
+            {
+                id: 2,
+                name: "Item 2",
+                description: "Description 2",
+                status: "trade",
+                userId: 11
+            },
+            {
+                id: 3,
+                name: "Item 3",
+                description: "Description 3",
+                status: "trade",
+                userId: 11
+            },
+        ],
+    },
+    {
+        id: 22,
+        name: "Mary Jane",
+        email: "jane.mary@example.com",
+        items: [
+            {
+                id: 4,
+                name: "Item 4",
+                description: "Description 4",
+                status: "trade",
+                userId: 22
+            },
+            {
+                id: 5,
+                name: "Item 5",
+                description: "Description 5",
+                status: "normal",
+                userId: 22
+            },
+            {
+                id: 6,
+                name: "Item 6",
+                description: "Description 6",
+                status: "normal",
+                userId: 22
+            },
+        ],
+    },
+    {
+        id: 33,
+        name: "Bob marley",
+        email: "bob.marley@example.com",
+        items: [
+            {
+                id: 7,
+                name: "Item 7",
+                description: "Description 7",
+                status: "trade",
+                userId: 33
+            },
+            {
+                id: 8,
+                name: "Item 8",
+                description: "Description 8",
+                status: "trade",
+                userId: 33
+            },
+            {
+                id: 9,
+                name: "Item 9",
+                description: "Description 9",
+                status: "normal",
+                userId: 33
+            },
+        ],
+    },
+];
