e1062061f86776b56a590b43019ea9a4608db062
Nita1001 <devinedevops@gmail.com>
2023-04-19 02:46:18 +0300

    useInventory

--- client/src/hooks/useInventory.js
+++ client/src/hooks/useInventory.js
@@ -1,148 +1,184 @@
-import { useState, useEffect } from "react";
-import { items1 } from "../components/items";
-import { users } from "../components/users";
+import { useState, useContext, useEffect } from "react";
 import { getItems, getItem } from '../api/InventoryServices';
+import { SelectedUserContext } from "../contexts/SelectedUserContext";
+import { InventoryContext } from "../contexts/InventoryContext";
+
 const useInventory = () => {
-    const [items, setItems] = useState(items1);
-    const [tradingItems, setTradingItems] = useState([]);
-    const [selectedUser, setSelectedUser] = useState(null);
-    const [selectedTradingItem, setSelectedTradingItem] = useState(null);
-    const [selectedOfferItem, setSelectedOfferItem] = useState([]);
-    const [userTradingItems, setUserTradingItems] = useState([]);
+    const { selectedUser, handleUserSelected } = useContext(SelectedUserContext);
+    
+    const [currentUserItems, setCurrentUserItems] = useState([]);
+    const [selectedUserInventory, setSelectedUserInventory] = useState([]);
+    const [selectedUserItems, setSelectedUserItems] = useState([]);
+
+    const [selectedOfferItems, setSelectedOfferItems] = useState([]);
+    const [requestedItems, setRequestedItems] = useState([]);
     const [isConfirmed, setIsConfirmed] = useState(false);
-
-    useEffect(() => {
-        // Filter out the item with status trade of the selected user
-        const filteredItems = selectedUser
-            ? selectedUser.items.filter((item) => item.status === "trade")
-            : [];
-        console.log('filtered items', filteredItems);
-        setSelectedOfferItem([]);
-        setUserTradingItems(filteredItems);
-    }, [selectedUser]);
-
-    const handleAddToTrading = (itemId) => {
-        const itemToAdd = items.find((item) => item.id === itemId);
-        if (
-            itemToAdd.status === "trade" &&
-            !tradingItems.find((item) => item.id === itemId)
-        ) {
-            setSelectedTradingItem(itemToAdd);
-            setTradingItems([...tradingItems, itemToAdd]);
-        }
+    
+    const [tradingItems, setTradingItems] = useState([]);
+    const [selectedTradingItem, setSelectedTradingItem] = useState(null);
+    
+    const itemsOfSelectedUser = (items) => {
+        setSelectedUserItems(items);
     };
 
-    const handleRemoveItemFromTradingBox = (itemId) => {
-        setTradingItems((prevTradingItems) =>
-            prevTradingItems.filter((item) => item.id !== itemId)
-        );
+    const itemsOfCurrentUser = (items) => {
+        setCurrentUserItems(items);
     };
 
-    const getItemsOfSelectedUser = async (inventoryId) => {
+    const getItemsOfUser = async (inventoryId, user) => {
         const itemIdsObject = await getItems(inventoryId);
         const itemIds = itemIdsObject.items;
-        console.log('itemIds:', itemIds);
         const items = await Promise.all(itemIds.map(async (itemId) => {
-            const item = await getItem(itemId);
-            return item;
+            return await getItem(itemId);
         }));
-        console.log('items:', items);
+        user === 'currentUser' ? itemsOfCurrentUser(items) : itemsOfSelectedUser(items);
     }
 
-    const handleSelectUser = (user) => {
-        console.log('user', user);
-        const { firstName, lastName, inventoryId } = user;
-        console.log('firstName, lastName, inventoryId', firstName, lastName, inventoryId)
-        getItemsOfSelectedUser(inventoryId);
-        // setSelectedUser(user);
+    const fetchUserInventory = async (inventoryId) => {
+         await getItemsOfUser(inventoryId, 'currentUser');
+    }
+
+    useEffect(() => {
+        // Filtering items with status trading of selected user
+        const filteredItems = selectedUserItems
+            ? selectedUserItems.filter((items) => items.item.status === "trading")
+            : [];
+        setSelectedUserInventory(filteredItems);
+    }, [selectedUser, selectedUserItems]);
+
+    const handleRemoveOfferingItem = (itemId) => {
+        // console.log('itemId | handleRemove', itemId);
+        console.log('selectedOfferItem', selectedOfferItems);
+        // debugger
+        setSelectedOfferItems((prevTradingItems) =>
+            prevTradingItems.filter((item) => item._id !== itemId)
+        );
     };
 
-    const handleOfferItem = (selectedItem) => {
-        const isItemAlreadySelected = selectedOfferItem?.some(
-            (item) => item.id === selectedItem.id
+    const handleSelectUser = async (user) => {
+        const { firstName, lastName, inventoryId } = user;
+        await getItemsOfUser(inventoryId, 'selectedUser');
+        handleUserSelected(user);
+    };
+
+    const handleOfferedItem = (selectedItem) => {
+        const isItemAlreadySelected = selectedOfferItems?.some(
+            (item) => item._id === selectedItem._id
         );
         if (!isItemAlreadySelected) {
-            setSelectedOfferItem([...selectedOfferItem, selectedItem]);
+            setSelectedOfferItems([...selectedOfferItems, selectedItem]);
         } else {
             console.log("Item already selected!");
+            return;
         }
     };
 
-    const handleCancelTrade = () => {
+    const handleRequestedItem = (selectedItem) => {
+        const isItemAlreadySelected = requestedItems?.some(
+            (item) => item._id === selectedItem._id
+        );
+        if (!isItemAlreadySelected) {
+            setRequestedItems([...requestedItems, selectedItem]);
+        } else {
+            console.log("Item already selected!");
+            return;
+        }
+    };
+
+    const handleRemoveRequestedItem = (itemId) => {
+        setRequestedItems((prevTradingItems) => {
+            const filteredItems = prevTradingItems.filter((item) => item._id !== itemId);
+            return filteredItems;
+        });
         setIsConfirmed(false);
-        setSelectedOfferItem([]);
-        setTradingItems([]);
     };
 
     const handleConfirmTrade = () => {
-        setSelectedTradingItem([]);
-        setSelectedOfferItem([]);
         setIsConfirmed(true);
         // if (selectedOfferItem.length > 0 && selectedTradingItem.length > 0) {
-        //     setIsConfirmed(true);
-        //     setTradingItems([...selectedOfferItem, ...selectedTradingItem]);
-        // }
-    };
+            //     setIsConfirmed(true);
+            //     setTradingItems([...selectedOfferItem, ...selectedTradingItem]);
+            // }
+        // setSelectedTradingItem([]);
+        // setSelectedOfferItem([]);
+        };
 
     const handleCompleteTrade = () => {
         console.log("TRADE COMPLETED !");
 
-        if (isConfirmed) {
-            const newItems = items.map((item) => {
-                if (selectedOfferItem?.id && item.id === selectedOfferItem.id) {
-                    return { ...item, status: "normal" };
-                } else if (
-                    selectedTradingItem?.id &&
-                    item.id === selectedTradingItem.id
-                ) {
-                    return { ...item, status: "normal" };
-                } else {
-                    return item;
-                }
-            });
-            setItems(newItems);
-            setTradingItems([]);
-            setSelectedTradingItem(null);
-            setSelectedOfferItem([]);
-            setIsConfirmed(false);
-        }
+        // if (isConfirmed) {
+        //     const newItems = items.map((item) => {
+        //         if (selectedOfferItems?._id && item._id === selectedOfferItems._id) {
+        //             return { ...item, status: "normal" };
+        //         } else if (
+        //             selectedTradingItem?.id &&
+        //             item._id === selectedTradingItem._id
+        //         ) {
+        //             return { ...item, status: "normal" };
+        //         } else {
+        //             return item;
+        //         }
+        //     });
+        //     setItems(newItems);
+        //     // setTradingItems([]);
+        //     setSelectedTradingItem(null);
+        //     setSelectedOfferItems([]);
+        //     setIsConfirmed(false);
+        // }
     };
 
-    const handleTradingItem = (selectedItem) => {
-        const isItemAlreadySelected = selectedTradingItem.some(
-            (item) => item.id === selectedItem.id
-        );
-        if (!isItemAlreadySelected) {
-            const itemToTrade = items.find(
-                (item) => item.id === selectedItem.id
-            );
-            setSelectedTradingItem([...selectedTradingItem, itemToTrade]);
-        } else {
-            console.log("Item already selected!");
-        }
-    };
+  
     return {
-        users,
-        items,
+        
         selectedUser,
-        tradingItems,
-        selectedOfferItem,
-        selectedTradingItem,
-        userTradingItems,
-        isConfirmed,
-        setItems,
-        setTradingItems,
-        setSelectedTradingItem,
-        setSelectedOfferItem,
+        currentUserItems,
+        selectedUserInventory,
         handleSelectUser,
-        handleAddToTrading,
-        handleRemoveItemFromTradingBox,
-        handleTradingItem,
+        fetchUserInventory,
+        
+        requestedItems,
+        selectedOfferItems,
+        setSelectedOfferItems,
+        handleRemoveOfferingItem,
+        handleRemoveRequestedItem,
+        
+        handleOfferedItem,
+        handleRequestedItem,
+        
         handleCompleteTrade,
         handleConfirmTrade,
-        handleCancelTrade,
-        handleOfferItem,
+        
+        isConfirmed,
+        // tradingItems,
+        // setTradingItems,
+        // handleAddToTrading,
+        // selectedTradingItem,
+        // setSelectedTradingItem,
     };
 };
 
 export default useInventory;
+
+// 444 halfway through
+
+   // const [currentUserTradingItems, setCurrentUserTradingItems] = useState([]);
+    // 
+    // const handleAddToTrading = async (itemId) => {
+    //     console.log('Adding item to tradingItems array');
+    //     const itemToAdd = await getItem(itemId);
+    //     console.log('itemToAdd', itemToAdd);
+    //     if (!itemToAdd) {
+    //         console.warn(`Item with id ${itemId} is not in trading status or cant be found`);
+    //         // debugger
+    //         return;
+    //     }
+
+    //     if (currentUserTradingItems.some((item) => item.id === itemId)) {
+    //         console.warn(`Item with id ${itemId} is already in tradingItems array`, currentUserTradingItems);
+    //         // debugger
+    //         return;
+    //     }
+
+    //     setSelectedTradingItem(itemToAdd);
+    //     setTradingItems((prevItems) => [...prevItems, itemToAdd]);
+    // };
