65024868d4f14649e57489940a685181ab8ada69
Nita1001 <devinedevops@gmail.com>
2023-04-19 02:43:19 +0300

    TradingBox

--- client/src/components/TradingBox.jsx
+++ client/src/components/TradingBox.jsx
@@ -1,77 +1,46 @@
 import React, { useContext } from "react";
 import uniqid from "uniqid";
-import UserTradingItems from "./UserTradingItems";
-import OfferContainer from "./OfferContainer";
+
 import { InventoryContext } from "../contexts/InventoryContext";
+
+import ItemsInTradingBox from "./ItemsInTradingBox";
+// import TradeConfirmation from "./TradeConfirmation";
 
 import "./styles/TradingBox.style.css";
 
 const TradingBox = () => {
     const inventory = useContext(InventoryContext);
-
     const {
-        selectedOfferItem,
-        tradingItems,
-        handleRemoveItemFromTradingBox,
-        handleCompleteTrade,
-        handleConfirm,
-        handleOfferItem,
-        userTradingItems,
-        isConfirmed,
+        handleConfirmTrade,
+        // handleCompleteTrade,
+        // handleCancelTrade,
+        // isConfirmed,
     } = inventory;
 
     return (
         <>
-            <div className="trading-container">
-                <UserTradingItems userTradingItems={userTradingItems} />
-            </div>
-            <h4>Your selected trading items:</h4>
-            <div className="trading-items-container">
-                {tradingItems.map((item) => (
-                    <div key={item.id} className="trading-item">
-                        <h5>{item.name}</h5>
-                        <button
-                            onClick={() =>
-                                handleRemoveItemFromTradingBox(item.id)
-                            }
-                        >
-                            Remove
-                        </button>
-                        <button onClick={() => handleOfferItem(item)}>
-                            offer for trade
-                        </button>
-                    </div>
-                ))}
-            </div>
             <div className="offer-container">
-                <OfferContainer />
+                <ItemsInTradingBox />
             </div>
             <div className="confirm-trade">
-                {selectedOfferItem?.length > 0 && (
-                    <>
-                        <h4>Selected Items of User B</h4>
-                        {selectedOfferItem.map((item) => (
-                            <div key={uniqid()} className="trading-item">
-                                <h5>{item && item.name}</h5>
-                                <p>{item && item.description}</p>
-                            </div>
-                        ))}
-
-                        <button onClick={handleConfirm}>Confirm Trade</button>
-                        <p>Waiting for other user to confirm...</p>
-                    </>
-                )}
-                {!isConfirmed && (
+                <button onClick={handleConfirmTrade}>Confirm Trade</button>
+                <p>Waiting for User B to confirm...</p>
+                {/* {isConfirmed && (
                     <>
                         <button onClick={handleCompleteTrade}>
                             Complete Trade
                         </button>
+                        <button onClick={() => handleCancelTrade("ok")}>
+                            Cancel Trade
+                        </button>
                         <p>
-                            Both users have confirmed. Click 'Complete Trade' to
+                            Both users have confirmed. 'Complete Trade' to
                             finalize.
                         </p>
                     </>
-                )}
+                )} */}
+                {/* <TradeConfirmation /> */}
+                {/* socket io */}
             </div>
         </>
     );
