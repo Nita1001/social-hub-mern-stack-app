52c4ddd463becaae86c411912da538f448770fa2
Nita1001 <devinedevops@gmail.com>
2023-04-19 02:48:10 +0300

    trying socket with currentUserId

--- client/src/events/socket.js
+++ client/src/events/socket.js
@@ -1,6 +1,28 @@
+// import { io } from 'socket.io-client';
+
+// import { SOCKET_SERVER_URL } from './constants/server.js';
+
+// export const socket = io(SOCKET_SERVER_URL);
+
+// ----------------- 
+
+import { useContext, useMemo } from 'react';
 import { io } from 'socket.io-client';
+import { LoginContext } from '../contexts/LoginContext';
 
 import { SOCKET_SERVER_URL } from './constants/server.js';
 
-export const socket = io(SOCKET_SERVER_URL);
+export const useSocket = () => {
+    const { userId } = useContext(LoginContext);
+    const socket = useMemo(
+        () =>
+            io(SOCKET_SERVER_URL, {
+                query: {
+                    clientId: userId,
+                },
+            }),
+        [userId]
+    );
 
+    return socket;
+};
