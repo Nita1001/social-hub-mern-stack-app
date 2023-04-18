ae49c1330588eef935b27407d0f9fd6c240c3db1
Nita1001 <devinedevops@gmail.com>
2023-04-19 02:48:23 +0300

    isSelectedUser

--- client/src/contexts/SelectedUserContext.jsx
+++ client/src/contexts/SelectedUserContext.jsx
@@ -1,37 +1,62 @@
 import React, { createContext, useReducer } from "react";
 import usersReducer from "../reducers/usersReducer.js";
+import { selectedUserActions } from "../constants/selectedUserActions";
 
-const SET_SELECTED_USER = "SET_SELECTED_USER";
-const CLEAR_SELECTED_USER = "CLEAR_SELECTED_USER";
-
-const SelectedUserContext = createContext({ selectedUser: {} });
+const SelectedUserContext = createContext({
+    selectedUser: {},
+    isSelectedUser: false,
+});
 
 const SelectedUserProvider = ({ children }) => {
-    const initialState = { selectedUser: {} };
+    const initialState = {
+        selectedUser: {},
+        isSelectedUser: false,
+    };
+
     const [state, dispatch] = useReducer(usersReducer, initialState);
 
-    const handleUserSelected = (user) => {
+    const toggleIsSelectedUser = () => {
         dispatch({
-            type: SET_SELECTED_USER,
+            type: selectedUserActions.SET_IS_SELECTED_USER,
+            payload: !state.isSelectedUser,
+        });
+    };
+
+    const handleUserSelected = (user) => {
+        toggleIsSelectedUser();
+        dispatch({
+            type: selectedUserActions.SET_SELECTED_USER,
             payload: user,
         });
+        // dispatch({
+        //     type: selectedUserActions.SET_IS_SELECTED_USER,
+        //     payload: true,
+        // });
         const selectedUserData = { _id: user._id, username: user.username };
         localStorage.setItem(
             "selectedUserData",
             JSON.stringify(selectedUserData)
         );
+        localStorage.setItem("isSelectedUser", JSON.stringify(true));
     };
 
     const clearSelectedUser = () => {
         console.log("cleared", state.selectedUser);
-        dispatch({ type: CLEAR_SELECTED_USER });
-        localStorage.setItem("selectedUserData", JSON.stringify(NULL));
+        dispatch({ type: selectedUserActions.CLEAR_SELECTED_USER });
+        localStorage.setItem("selectedUserData", JSON.stringify(null));
+        localStorage.setItem("isSelectedUser", JSON.stringify(false));
+
+        dispatch({
+            type: selectedUserActions.SET_IS_SELECTED_USER,
+            payload: false,
+        });
     };
 
     return (
         <SelectedUserContext.Provider
             value={{
                 selectedUser: state.selectedUser,
+                isSelectedUser: state.isSelectedUser,
                 handleUserSelected,
                 clearSelectedUser,
             }}
