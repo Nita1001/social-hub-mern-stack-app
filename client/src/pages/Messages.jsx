1be44b1e532c70e95d30ba38dafb4906a24b8c56
Nita1001 <devinedevops@gmail.com>
2023-04-19 06:56:32 +0300

    backgrounds

--- client/src/pages/Messages.jsx
+++ client/src/pages/Messages.jsx
@@ -1,15 +1,25 @@
-import React from "react";
+import React, { useEffect } from "react";
 import Conversation from "../components/Conversation";
 import UsersList from "../components/UsersList";
 
+import "./styles/Home.style.css";
 import "./styles/Messages.style.css";
 
 const Messages = () => {
+    useEffect(() => {
+        document.body.classList.add("messages-page");
+        return () => {
+            document.body.classList.remove("messages-page");
+        };
+    }, []);
+
     return (
-        <div className="UserMessages-container">
-            <UsersList />
-            <Conversation />
-        </div>
+        <>
+            <div className="UserMessages-container">
+                <UsersList />
+                <Conversation />
+            </div>
+        </>
     );
 };
 
--- client/src/pages/Storage.jsx
+++ client/src/pages/Storage.jsx
@@ -1,7 +1,14 @@
-import React from "react";
+import React, { useEffect } from "react";
 import Inventory from "../components/Inventory";
 
 const Storage = () => {
+    useEffect(() => {
+        document.body.classList.add("storage-page");
+        return () => {
+            document.body.classList.remove("storage-page");
+        };
+    }, []);
+
     return <Inventory />;
 };
 export default Storage;
