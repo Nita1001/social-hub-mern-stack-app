56ef63720a30519606ae14184ee49e3203a52d78
Nita1001 <devinedevops@gmail.com>
2023-04-19 06:53:22 +0300

    darkMode

--- client/src/components/Message.jsx
+++ client/src/components/Message.jsx
@@ -1,13 +1,20 @@
-import React from "react";
+import React, { useContext } from "react";
 import { dateFormatter } from "../utils/dateFormatter.js";
 import "./styles/conversation.style.css";
+import { DarkModeContext } from "../contexts/DarkModeContext.jsx";
 
 const Message = ({ message, sentByCurrUser }) => {
+    const isDarkMode = useContext(DarkModeContext);
+
     const messageClassName = sentByCurrUser
         ? "message sent"
         : "message received";
     const messageTimeClassName = sentByCurrUser
-        ? "time sent-time"
+        ? isDarkMode
+            ? "sent-time darkMode-item"
+            : "time sent-time"
+        : isDarkMode
+        ? "received-time darkMode-item"
         : "time received-time";
     const sentOrReceived = sentByCurrUser ? "sent" : "received";
 
