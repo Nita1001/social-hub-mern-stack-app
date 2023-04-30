09259c8176be1c62fd63573b04470c8b52a0017e
Nita1001 <devinedevops@gmail.com>
2023-04-30 13:01:45 +0300

    server url

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,16 +1,18 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+const serverUrl = process.env.SERVER_URL || 'http://localhost:5000';
+
 export default defineConfig({
   plugins: [react()],
   server: {
     proxy: {
       '/api/': {
-        target: 'http://localhost:5000',
+        target: serverUrl,
         changeOrigin: true,
       },
       '/socket.io/': {
-        target: 'http://localhost:5000',
+        target: serverUrl,
         ws: true,
       }
     }
